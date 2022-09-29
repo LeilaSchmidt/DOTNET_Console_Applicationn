23f2caed28d9567240042e12ca168936d3c62937
Leila Schmidt <le.schmidt@douglas.de>
2022-09-29 17:07:01 +0200

    changes2

--- CRUDDouglas/Program.cs
+++ CRUDDouglas/Program.cs
@@ -5,7 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hi from Witten!");
+            Console.WriteLine("Please insert Title");
+            var Title = Console.ReadLine();
+
+            Console.WriteLine("Please insert Price");
+            var Price = Console.ReadLine();
+
+            var ConvertedPrice = Convert.ToInt32(Price);
+            var WishListItem = new WhishlistItem();
+            WishListItem.Title = Title;
+            Console.WriteLine(WishListItem.Title);
         }
     }
 }
---
+++ CRUDDouglas/WhishlistItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDDouglas
+{
+    public class WhishlistItem
+    {
+        private string title;
+
+        public string Title
+        {
+            get
+            {
+                return title + "2";
+            }
+            set => title = value + "hallo";
+        }
+        public int Price { get; set; }
+    }
+}
